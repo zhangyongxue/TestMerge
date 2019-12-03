ebba576603341e7e0aa375f94e11a31de82446ec
zyx <710947694@qq.com>
2019-12-03 17:21:37 +0800

    first commit and push

---
+++ MyTestABC/app/src/main/java/com/exmple/mytestabc/MainActivity.java
@@ -0,0 +1,15 @@
+package com.exmple.mytestabc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("hello master");
+    }
+}
