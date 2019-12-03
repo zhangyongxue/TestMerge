dacd150561464ff9f078d70bccf958c15647ffe0
zyx <710947694@qq.com>
2019-12-03 18:07:42 +0800

    dev2 add Handler code

--- MyTestABC/app/src/main/java/com/exmple/mytestabc/MainActivity.java
+++ MyTestABC/app/src/main/java/com/exmple/mytestabc/MainActivity.java
@@ -1,8 +1,11 @@
 package com.exmple.mytestabc;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,12 +13,27 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
+
+    private class MyHandler extends Handler {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            switch (msg.what) {
+                case 0:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("hello master");
-        Button dev1 = new Button(this);
+        MyHandler myHandler = new MyHandler();
+        myHandler.sendEmptyMessage(0);
+        Button dev2 = new Button(this);
         Log.d(TAG, "onCreate: ");
     }
 }
