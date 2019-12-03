d6708596db9539cede30ae706e12ce96ff832f15
zyx <710947694@qq.com>
2019-12-03 17:39:39 +0800

    dev2 second commit and push

--- MyTestABC/app/src/main/java/com/exmple/mytestabc/MainActivity.java
+++ MyTestABC/app/src/main/java/com/exmple/mytestabc/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -14,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("hello master");
-        Button dev2 = new Button(this);
+        TextView dev2 = new TextView(this);
         Log.d(TAG, "onCreate: ");
     }
 }
