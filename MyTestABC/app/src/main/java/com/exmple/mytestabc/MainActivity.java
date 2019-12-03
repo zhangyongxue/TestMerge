f021104ddef040cbd3f0d594d5704c739f752953
zyx <710947694@qq.com>
2019-12-03 18:09:42 +0800

    dev1 add Button click listener

--- MyTestABC/app/src/main/java/com/exmple/mytestabc/MainActivity.java
+++ MyTestABC/app/src/main/java/com/exmple/mytestabc/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,5 +18,11 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("hello master");
         Button dev1 = new Button(this);
         Log.d(TAG, "onCreate: ");
+        dev1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("This is dev1 btn onclick listener");
+            }
+        });
     }
 }
