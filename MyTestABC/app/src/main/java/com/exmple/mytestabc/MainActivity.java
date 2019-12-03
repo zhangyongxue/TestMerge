a72092a00785247643e36258c6e47909037d3ab8
zyx <710947694@qq.com>
2019-12-03 17:28:04 +0800

    dev1 first commit and push

--- MyTestABC/app/src/main/java/com/exmple/mytestabc/MainActivity.java
+++ MyTestABC/app/src/main/java/com/exmple/mytestabc/MainActivity.java
@@ -3,13 +3,18 @@ package com.exmple.mytestabc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("hello master");
+        Button dev1 = new Button(this);
+        Log.d(TAG, "onCreate: ");
     }
 }
