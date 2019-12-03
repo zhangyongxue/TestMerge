77ca3e1d72681070e354cec7f2bde55c69b76318
zyx <710947694@qq.com>
2019-12-03 18:39:44 +0800

    dev2 remove button

--- MyTestABC/app/src/main/java/com/exmple/mytestabc/MainActivity.java
+++ MyTestABC/app/src/main/java/com/exmple/mytestabc/MainActivity.java
@@ -1,15 +1,12 @@
 package com.exmple.mytestabc;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -34,13 +31,6 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("hello master");
         MyHandler myHandler = new MyHandler();
         myHandler.sendEmptyMessage(0);
-        Button dev2 = new Button(this);
-        Log.d(TAG, "onCreate: ");
-        dev1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.out.println("This is dev1 btn onclick listener");
-            }
-        });
+
     }
 }
